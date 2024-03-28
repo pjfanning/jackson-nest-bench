82e84a5e7ce23bc21ed04ba131db230222c8377a
PJ Fanning <pjfanning@users.noreply.github.com>
2024-03-28 15:36:53 +0100

    Update InternCacheBench.java

--- src/jmh/java/org/example/jackson/bench/InternCacheBench.java
+++ src/jmh/java/org/example/jackson/bench/InternCacheBench.java
@@ -5,17 +5,31 @@ import org.openjdk.jmh.infra.Blackhole;
 
 import com.fasterxml.jackson.core.util.InternCache;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InternCacheBench extends BenchmarkLauncher {
 
-    private static final String UUID = java.util.UUID.randomUUID().toString();
+    private static final List<String> UUIDs = new ArrayList<>();
+    private static final int SIZE = 10000;
+
+    static {
+        for (int i = 0; i < SIZE; i++) {
+            UUIDs.add(java.util.UUID.randomUUID().toString());
+        }
+    }
 
     @Benchmark
     public void intern(Blackhole blackhole) throws Exception {
-        InternCache.instance.intern(UUID);
+        for (String UUID : UUIDs) {
+            blackhole.consume(InternCache.instance.intern(UUID));
+        }
     }
 
     @Benchmark
     public void internNewLock(Blackhole blackhole) throws Exception {
-        InternCacheNewLock.instance.intern(UUID);
+        for (String UUID : UUIDs) {
+            blackhole.consume(InternCacheNewLock.instance.intern(UUID));
+        }
     }
 }
